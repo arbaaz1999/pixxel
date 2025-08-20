6ac54f3d0db683a96c1a465357379e0c40b3a485
arbaaz1999 <arbaazmansuri09@gmail.com>
2025-08-20 16:53:58 +0530

    Pixxel App Initiated and Auth Added

---
+++ app/(auth)/layout.js
@@ -0,0 +1,7 @@
+export default function AuthLayout({ children }) {
+    return (
+        <div className="flex justify-center py-[15%]">
+            {children}
+        </div>
+    )
+}
---
+++ components/Header.jsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import { Button } from './ui/button'
+
+const Header = () => {
+    const pathName = usePathname()
+    return (
+        <header className='fixed top-[6%] left-1/2 transform -translate-1/2 z-50 text-nowrap'>
+            <div className='backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-8 py-3 flex items-center justify-between gap-8'>
+                <Link href="/" className='mr-10 md:mr-20'>
+                    <Image
+                        src="/logo-text.png"
+                        alt='Pixxel Logo'
+                        className='min-w-24 object-cover'
+                        width={96}
+                        height={24}
+                        quality={100}
+                    />
+                </Link>
+                {pathName === "/" && (
+                    <div className='hidden md:flex space-x-6'>
+                        <Link href="#features" className='text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer'>
+                            Features
+                        </Link>
+                        <Link href="#pricing" className='text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer'>
+                            Pricing
+                        </Link>
+                        <Link href="#contact" className='text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer'>
+                            Contact
+                        </Link>
+                    </div>
+                )}
+                <div className='flex items-center gap-3 ml-10 md:ml-20'>
+                    <SignedOut>
+                        <SignInButton>
+                            <Button variant="glass" className="hidden sm:flex">Sign In</Button>
+                        </SignInButton>
+                        <SignUpButton>
+                            <Button variant="primary">Get Started</Button>
+                        </SignUpButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton appearance={{
+
+                        }} />
+                    </SignedIn>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
